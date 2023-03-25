1284905b366c845cd60765c74f347153845e40af
padonan <bbde1861@gmail.com>
2023-03-26 08:01:07 +0900

    03 26

--- promise1.js
+++ promise1.js
@@ -7,3 +7,13 @@ function getHighScore() {
     .then(profile => profile.highScore);
 }
 
+function printWithDelay(print, timeout) {
+  //2.`setTimeout`을 이용해서 비동기 처리 함수를 완성합니다.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("async callback");
+        resolve();
+    }, 3000)
+  });
+}
+
